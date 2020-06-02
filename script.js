9d35a61b7bc98395bb184074ff4f1663a4908790
Solomon Gold <solomon@sigfried.org>
2020-06-02 16:39:40 -0400

    I added a bunch of stuff. Level one exists and they move. Contact does not totally work. I am not sure why.

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
 
 function init() {
- 
+  $$('#startstop').addEvent("click",beginOne)
+}
+
+function makeScaleConvert(d0,d1,r0,r1){
+  return function(dn){
+    dRange = (d1 - d0)  
+    rRange = (r1 - r0)  
+    rValue = (((dn - d0) * rRange) / dRange) + r0
+    return (rValue)
+
+    //(10°F − 32) × 5/9 = -12.22°C
+
+    //return (d1-d0)/(r1-r0)*(dn)-(d0-r0)
+  }
+}
+function beginOne(){
+  
   conf = getConfig()
   let s1 = new Space()
-  let joe = new Thing({...conf.player, space: s1})
+  let joe = new Politician({...conf.player, space: s1})
+  let firstEnemies = conf.candidates
+  firstD = {}
+  _.forEach(firstEnemies,d=>  firstD[d.name] = new Candidate({...d, space : s1}))
+
+  console.log(firstD)
   document.onkeydown= evt => {
     let speed = conf.directions[evt.keyCode] || joe.speed
     joe.changeSpeed(speed)
   }
+  function unpauser(){
+    s1.interval = setInterval(s1.advanceTime.bind(s1), 50)
+    $$('#startstop').set("text","pause")
+    $$('#startstop').removeEvent("click", unpauser);
+    $$('#startstop').addEvent("click",pauser)
+    
+  }
+  function pauser(space) {
+    clearInterval(s1.interval)
+    $$('#startstop').set("text","unpause")
+
+    $$('#startstop').removeEvent("click", pauser);
+    $$('#startstop').addEvent("click",unpauser)
+  }
+  $("l0").setStyle('display', 'none')  
+  $$('#startstop').removeEvent("click", beginOne);
+  $$('#startstop').addEvent("click",pauser)
+  $$('#startstop').set("text","pause")
+
 }
+
+
 class Space {
   constructor() {
     this.things = []
@@ -16,35 +58,100 @@ class Space {
     this.height = window.innerHeight
     this.w = this.width / 2
     this.h = this.height / 2
-    setInterval(this.advanceTime.bind(this), 50)
+    this.interval = setInterval(this.advanceTime.bind(this), 50)
+    this.xScale = makeScaleConvert(-this.w, this.w, 0, this.width)
+    this.yScale = makeScaleConvert(-this.h, this.h, this.height, 0)
+
   }
   injectThing(thing) {
     this.things.push(thing)
     let el = new Element('img',{
       src: thing.src,
       id: thing.id,
+      "class": thing.classes,
     })
     el.inject(document.body)
     thing.el = el
     this.renderThing(thing)
   }
   advanceTime() {
+    this.checkForCollisions()
     this.things
       .filter(t => t.move())
+      .map(t => this.checkAndDealWithWalllHit(t))
       .map(t => this.renderThing(t))
+    
     //this.checkForCollisions()
   }
-  renderThing(thing) {
-    thing.el.style.left = (thing.pos.x + this.w) + 'px'
-    thing.el.style.top  = (thing.pos.y + this.h) + 'px'
+  heightBounce(thing){
+    if (Math.abs(thing.hFromCenter)>=this.h){
+      thing.changeSpeed({x:0,y:-thing.speed.y})
+      
+    }
+    if ("swallwell" == thing.id){
+      console.log(thing.hFromCenter)
+    }
+
+    
+    
   }
+  
+  checkAndDealWithWalllHit(thing) {
+    this.heightBounce(thing)
+    return(thing)
+}
+  renderThing(thing) {
+    thing.el.style.left = this.xScale(thing.pos.x) + 'px'
+    thing.el.style.top  = this.yScale(thing.pos.y+thing.h) + 'px'
+  }
+  edgePos(thing, whichEdge) {
+    if (whichEdge === 't') return thing.pos.y - thing.h
+    if (whichEdge === 'b') return thing.pos.y + thing.h
+    if (whichEdge === 'l') return thing.pos.y - thing.h
+    if (whichEdge === 'r') return thing.pos.y + thing.h
+  }
+  spaceCoords2winCoords(pos) {
+    
+  }
+  
+
+
+
+  /*
+  d3xScale() {
+    return d3.linearScale().domain([-this.w, this.w]).range([0, this.width])
+  }
+  d3yScale() {
+    return d3.linearScale().domain([-this.h, this.h]).range([this.height, 0])
+  }*/
   endOfTime() {
     this.things.forEach(t => t.disappear())
   }
   checkForCollisions() {
-    let colliders = this.things.filter()
+    let colliders = this.things
+    colliders.forEach(thing =>this.iCollided(thing,colliders))
+
+  }
+  iCollided(thing,colliders){
+    colliders.slice(colliders.indexOf(thing)+1,).forEach(d => this.weCollided(thing,d))
+  
+  }
+  weCollided(thing,oThing){
+    
+    if (Math.abs(thing.pos.x-oThing.pos.x)>(thing.w+oThing.w)){
+      return(false)
+
+    }
+    if (Math.abs(thing.pos.y-oThing.pos.y)>(thing.h)){
+      return(false)
+
+    }
+    thing.collide(oThing)
+    oThing.collide(thing)
+
   }
 }
+
 class Thing {
   constructor(props) {
     this.space = props.space
@@ -56,8 +163,13 @@ class Thing {
     this.h = this.size.height / 2
     this.speed = {...props.speed}
     this.shootableThing = props.shootableThing || []
+    this.classes = props.classes
     this.space.injectThing(this)
+    this.hFromCenter = (Math.abs(this.pos.y)+this.h)*((this.pos.y)/Math.abs(this.pos.y))
   }
+ 
+  
+
   render() {
 
   }
@@ -65,8 +177,10 @@ class Thing {
     if (this.speed.x == 0 && this.speed.y == 0) {
       return false
     }
+    
     this.pos.x += this.speed.x
     this.pos.y += this.speed.y
+    this.hFromCenter=(Math.abs(this.pos.y)+this.h)*((this.pos.y)/Math.abs(this.pos.y))
     return true
   }
   changeSpeed(speed) {
@@ -76,8 +190,17 @@ class Thing {
 
   }
 }
-class Candidate extends Thing {
-  constructor(...props) {
+class Politician extends Thing {
+  constructor(props) {
+    super(props)
+  }
+  collide(oThing){
+    this.speed.x = oThing.speed.x
+    this.speed.y = oThing.speed.y
+  }
+}
+class Candidate extends Politician {
+  constructor(props) {
     super(props)
   }
   shoot() {
@@ -91,7 +214,7 @@ let bankScore=0;
 let newScore = 0;
 pauser = document.querySelector("#startstop")
 let lives=4
-let joe = document.querySelector("#joe");
+let  = document.querySelector("#joe");
 
 
 cands = ['swallwell','hickenlooper','inslee','gillibrand']
@@ -225,7 +348,7 @@ function act(kc,d){
   canDic["model"]["y"] += canDic["speed"]
   canDic["model"]["yEnd"] += canDic["speed"]
   canDic["id"].style.top = canDic["model"]["y"]+"px"
-}*/
+}
 function candidateMove(canDic){
   canDic["el"].style.top = "20px"
   if ((canDic["model"]["y"] <=0) || (canDic["model"]["y"] >= scrHeight)){
@@ -297,47 +420,57 @@ function getConfig() {
     src: "joeBiden.jpeg",
     size: { width: 100, height: 60, },
     speed: {x: 0, y: 0},
+    classes: 'character',
   }
 
   const candidates = {
     swallwell: {
       name: "Eric Swallwell",
+      id:"swallwell",
       src: "swallwell.jpeg",
       level: 1,
-      start: { x:250, y:50, },
+      start: { x:-450, y:50, },
       size: { width: 100, height: 60, },
-      speed: {x: -1, y: 0},
+      speed: {x: 0, y: -1},
+      classes: 'character',
     },
     hickenlooper: {
       name: "John Hickenlooper",
+      id:"hickenlooper",
       src: "hickenlooper.jpeg",
       level: 1,
-      start: { x:550, y:50, },
+      start: { x:-200, y:0, },
       size: { width: 100, height: 60, },
-      speed: {x: -2, y: 0},
+      speed: {x: 0, y: -2},
+      classes: 'character',
     },
     inslee: {
       name: "Jay Inslee",
+      id:"inslee",
       src: "inslee.jpg",
       level: 1,
-      start: { x:850, y:50, },
+      start: { x:50, y:0, },
       size: { width: 100, height: 60, },
-      speed: {x: -3, y: 0},
+      speed: {x: 0, y: -3},
+      classes: 'character',
     },
     gillibrand: {
       name: "Kirsten Gillibrand",
+      id:"gillibrand",
       src: "gillibrand.jpeg",
       level: 1,
-      start: { x:1150, y:50, },
+      start: { x:300, y:0, },
       size: { width: 100, height: 60, },
-      speed: {x: -4, y: 0},
+      speed: {x: 0, y: -4},
+      classes: 'character',
     },
   }
   const directions = {
-    "39": {x:  1, y:  0},
-    "37": {x: -1, y:  0},
-    "40": {x:  0, y:  1},
-    "38": {x:  0, y: -1},
+    "39": {x:  3, y:  0},
+    "37": {x: -3, y:  0},
+    "40": {x:  0, y:  -3},
+    "38": {x:  0, y: 3},
   }
   return {player, candidates, directions}
+
 }
